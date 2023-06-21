01645f8644800d4be51df5c6fcffd6535dd385ac
MatsiboraK <kmacibora50@gmail.com>
2023-06-21 21:31:37 +0700

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnBody() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Welcome to Postman Echo")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("Welcome to Postman Echo"));
+
+    }
+
+}
